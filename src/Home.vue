e87ac67fd7bd6c4465a0c01059720c2eec0f56b8
Tier511 <work@tier5.us>
2018-07-05 00:42:49 +0530

    used vue component

---
+++ src/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <p>server status {{ status }}</p>
+      <hr>
+      <button @click="changeStatus">changeStatus</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+          return {
+            status: 'Critical'
+          }
+        },
+        methods: {
+          changeStatus() {
+            this.status = 'Normal';
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
